adbe70486fd2edb6d2566e3f830a24a3b475cef1
ndemia <info@demia.me>
2023-03-07 11:32:45 +0100

    Clean up

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './footer.css';
 export default function Footer() {
   return (
     <footer className="footer">
-      <span className="footer__text">© Copyright Social Brothers - 2022</span>
+      <span className="footer__text">© Copyright</span>
     </footer>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,5 @@
 import './header.css';
 import { useLocation } from 'react-router-dom';
-import companyLogo from '../../assets/images/sb-logo.svg';
 import Nav from '../Nav/Nav';
 import MainTitle from '../MainTitle/MainTitle';
 
@@ -17,9 +16,6 @@ export default function Header() {
     <div className="header__wrapper">
       <header className="header">
         <div className="header__navigation-wrap">
-          <figure className="header__logo-wrapper">
-            <img className="header__logo" src={companyLogo} alt="Social Brothers logo" />
-          </figure>
           <Nav />
         </div>
         {title && <MainTitle title={title} />}
